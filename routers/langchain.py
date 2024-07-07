43e644f41b13ac667770ebbf75ec061016b601c0
unknown <judemin2087@naver.com>
2024-07-07 20:17:09 +0900

    [UPDATE] : UPDATED LangChain, JSON Parser

--- routers/langchain.py
+++ routers/langchain.py
@@ -1,5 +1,7 @@
 import os
+import re
 import torch
+import json
 from uuid import uuid4
 from dotenv import load_dotenv
 # from langchain_community.llms import HuggingFaceHub
@@ -37,51 +39,83 @@ def speech_to_text_modification(connection_uuid, converted_text):
     docs = vectordb.similarity_search(converted_text, k=3)
     context = " ".join([doc.page_content for doc in docs])
 
-    # 텍스트 수정을 위한 프롬프트 템플릿
-    correction_template = f"""
+    textData = f"""
     이전 음성 인식 결과:
     {context}
 
     현재 음성 인식 결과:
     {converted_text}
-
-    실시간 음성인식 결과를 더욱 매끄럽게 하기 위해 위 문장에 기반하여 아래 조건의 작업을 수행해주세요.
-    1. 이전 결과를 고려하여 현재 텍스트를 문법적으로 올바르게 수정해주세요.
-    2. 현재 음성 인식 결과에서 잡음이나 인식 오류를 제거해주세요.
-    3. 이전 음성 인식 결과과의 문맥을 고려하여 자연스럽게 연결되도록 현재 음성 인식 결과를 수정해주세요.
-    4. 답변은 한국어로 번역해주세요.
-    4-1. 단, 음성인식 결과에 타 언어로 된 전문용어가 들어가 있다면 한국어로 변역하지 말아주세요.
-    5. 추가적인 설명 없이 수정된 현재 음성 인식 결과만 제공해주세요.
     """
 
-    # PromptTemplate :  원시 사용자 입력을 더 나은 입력으로 변환
+    # PromptTemplate : 원시 사용자 입력을 더 나은 입력으로 변환
     # OutputParser : 채팅 메시지를 문자열로 변환하는 출력 구문 분석기
-    prompt1 = ChatPromptTemplate.from_template("[{korean_input}] translate the question into English. Don't say anything else, just translate it.")
+    prompt = ChatPromptTemplate.from_template("""
+        {textData}
+                                              
+        실시간 음성인식 결과를 더욱 매끄럽게 하기 위해 위 문장에 기반하여 아래 조건의 작업을 수행해주세요.
+        1. 이전 결과를 고려하여 현재 텍스트를 문법적으로 올바르게 수정해주세요.
+        2. 현재 음성 인식 결과에서 잡음이나 인식 오류를 제거해주세요.
+        3. 이전 음성 인식 결과의 문맥을 고려하여 자연스럽게 연결되도록 현재 음성 인식 결과를 수정해주세요.
+        4. 답변은 한국어로 번역해주세요.
+        4-1. 단, 음성인식 결과에 타 언어로 된 전문용어가 들어가 있다면 한국어로 변역하지 말아주세요.
+        5. 추가적인 설명 없이 수정된 현재 음성 인식 결과만 출력해주세요.
+        6. 문장에 부가 설명을 붙이지 말고 오로지 제공된 현재 음성인식 결과에서만 수정해주세요.
+
+        개선된 문장만을 "result" key의 value에 담아 JSON 형태로 제공해주세요
+        결과 외에는 그 어떤 텍스트도 답변하지 말아주세요
+    """)
+
     chain1 = (
-        prompt1 
+        prompt 
         | llm 
         | StrOutputParser()
     )
 
-    prompt2 = ChatPromptTemplate.from_messages([
-        ("system", "You are a helpful, professional assistant in korean university. answer the question in Korean"),
-        ("user", "{input}")
-    ])
-    chain2 = (
-        {"input": chain1}
-        | prompt2
-        | llm
-        | StrOutputParser()
-    )
+    # prompt2 = ChatPromptTemplate.from_messages([
+    #     ("system", "You are a helpful, professional assistant in korean university. answer the question in Korean"),
+    #     ("user", "{input}")
+    # ])
+    # chain2 = (
+    #     {"input": chain1}
+    #     | prompt2
+    #     | llm
+    #     | StrOutputParser()
+    # )
 
-    corrected_text = chain2.invoke({"korean_input":correction_template})
-    print(corrected_text)
+    corrected_text = chain1.invoke({"textData" : textData})
+    json_result = parse_JSON(corrected_text)
+
+    result_value = json_result.get('result')
+    if result_value:
+        print("Result value:", result_value)
+    else:
+        print("No 'result' key found in the JSON")
+        return None
 
     # 수정된 텍스트를 Chroma DB에 저장
     vectordb.add_texts(
-        texts=[corrected_text],
+        texts=[result_value],
         metadatas=[{"connection_uuid": connection_uuid}],
         ids=[str(uuid4())],
     )
 
-    return corrected_text
+    return result_value
+
+def parse_JSON(llm_response):
+    json_pattern = re.compile(r'{[^{}]*?}')
+
+    # LLM 응답에서 JSON 값 찾기
+    json_match = json_pattern.findall(llm_response)
+    
+    if json_match:
+        json_str = json_match[-1]
+        
+        try:
+            json_data = json.loads(json_str)
+            return json_data
+        except json.JSONDecodeError:
+            print("Invalid JSON format")
+            return None
+    else:
+        print("No JSON found in the LLM response")
+        return None
